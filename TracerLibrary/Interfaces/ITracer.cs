07b29f0711cbdd3ff2ea3c6c7ad040e7e6ff1098
Pavel Slyaz <sir.sevendart@gmail.com>
2021-09-11 22:40:28 +0300

    Added interface ITracer and class that implements ITracer, changed type of project

---
+++ TracerLibrary/Interfaces/ITracer.cs
@@ -0,0 +1,16 @@
+﻿using TracerLibrary.Structs;
+
+namespace MPP_1.Interfaces
+{
+    public interface ITracer 
+    {
+        // вызывается в начале замеряемого метода
+        void StartTrace();
+
+        // вызывается в конце замеряемого метода
+        void StopTrace();
+
+        // получить результаты измерений
+        TraceResult GetTraceResult();
+    }
+}
---
+++ TracerLibrary/Tracer.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using MPP_1.Interfaces;
+using TracerLibrary.Structs;
+
+namespace TracerLibrary
+{
+    public class Tracer : ITracer
+    {
+
+        public void StartTrace()
+        {
+            
+        }
+
+        public void StopTrace()
+        {
+            
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return new TraceResult();
+        }
+    }
+}
