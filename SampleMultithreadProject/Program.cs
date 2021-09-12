e6cf60ecbae44b437f8b785efc9f4eaed256cca5
Pavel Slyaz <sir.sevendart@gmail.com>
2021-09-12 12:20:16 +0300

    Create sample project, it works!

---
+++ SampleMultithreadProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using TracerLibrary;
+using TracerLibrary.Interfaces;
+
+namespace SampleMultithreadProject
+{
+    class Program
+    {
+        private static ITracer _tracer;
+        
+        static void MethodA()
+        {
+            _tracer.StartTrace();
+            MethodB();
+            Thread.Sleep(500);
+            _tracer.StopTrace();
+        }
+
+        static void MethodB()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(300);
+            _tracer.StopTrace();
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            _tracer = new Tracer();
+            MethodA();
+            MethodB();
+            var result = _tracer.GetTraceResult();
+        }
+    }
+}
