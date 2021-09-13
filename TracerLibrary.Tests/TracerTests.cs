90be34534dbfc210737c36881910a265109a24a3
Pavel Slyaz <sir.sevendart@gmail.com>
2021-09-13 18:16:40 +0300

    Added test with inner method

--- TracerLibrary.Tests/TracerTests.cs
+++ TracerLibrary.Tests/TracerTests.cs
@@ -18,16 +18,52 @@ namespace TracerLibrary.Tests
         [Test]
         public void OneThread_OneMethod_300Ms()
         {
+            //Arrange
+            void TestMethod()
+            {
+                _tracer.StartTrace();
+                Thread.Sleep(300);
+                _tracer.StopTrace();
+            }
+            
             //Act
-            _tracer.StartTrace();
-            Thread.Sleep(300);
-            _tracer.StopTrace();
+            TestMethod();
             
             //Assert
             var traceResult = _tracer.GetTraceResult();
             var time = traceResult.Threads[Thread.CurrentThread.ManagedThreadId].Time;
             var result = time.TotalMilliseconds is >= 300 and <= 310;
             Assert.IsTrue(result, "Traced time in one thread with one method is incorrect.");
+        }
+        
+        [Test]
+        public void Inner_Method_300Ms_500Ms()
+        {
+            //Arrange
+            void InnerMethod()
+            {
+                _tracer.StartTrace();
+                Thread.Sleep(500);
+                _tracer.StopTrace();
+            }
+
+            void OuterMethod()
+            {
+                _tracer.StartTrace();
+                Thread.Sleep(300);
+                InnerMethod();
+                _tracer.StopTrace();
+            }
+            
+            //Act
+            OuterMethod();
+            
+            //Assert
+            var traceResult = _tracer.GetTraceResult();
+            var time = traceResult.Threads[Thread.CurrentThread.ManagedThreadId].Time;
+            var result = time.TotalMilliseconds is >= 800 and <= 810;
+            
+            Assert.IsTrue(result, "Traced time in one thread with inner method is incorrect.");
         }
     }
 }
