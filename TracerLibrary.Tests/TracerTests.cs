f4ace05aae859c37c57b26fa37620124615caf0d
Pavel Slyaz <sir.sevendart@gmail.com>
2021-09-14 11:03:35 +0300

    Added multithread test, all tests have been refactored

--- TracerLibrary.Tests/TracerTests.cs
+++ TracerLibrary.Tests/TracerTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading;
 using NUnit.Framework;
 using TracerLibrary.Interfaces;
@@ -16,13 +15,16 @@ namespace TracerLibrary.Tests
         }
 
         [Test]
-        public void OneThread_OneMethod_300Ms()
+        public void Trace_OneThread_OneMethod_300Ms()
         {
             //Arrange
+            const int testingTime = 300;
+            const int acceptableDeviation = 50;
+            
             void TestMethod()
             {
                 _tracer.StartTrace();
-                Thread.Sleep(300);
+                Thread.Sleep(testingTime);
                 _tracer.StopTrace();
             }
             
@@ -32,25 +34,31 @@ namespace TracerLibrary.Tests
             //Assert
             var traceResult = _tracer.GetTraceResult();
             var time = traceResult.Threads[Thread.CurrentThread.ManagedThreadId].Time;
-            var result = time.TotalMilliseconds is >= 300 and <= 310;
-            Assert.IsTrue(result, "Traced time in one thread with one method is incorrect.");
+            Assert.GreaterOrEqual(time.TotalMilliseconds, testingTime, 
+                "Elapsed time must be equal or greater than expected.");
+            Assert.LessOrEqual(time.TotalMilliseconds, testingTime + acceptableDeviation, 
+                "Elapsed time must be equal or less than acceptable deviation.");
         }
         
         [Test]
-        public void Inner_Method_300Ms_500Ms()
+        public void Trace_InnerMethod_500Ms_OuterMethod_300Ms()
         {
             //Arrange
+            const int testingTimeForOuterMethod = 300;
+            const int testingTimeForInnerMethod = 500;
+            const int acceptableDeviation = 50;
+            
             void InnerMethod()
             {
                 _tracer.StartTrace();
-                Thread.Sleep(500);
+                Thread.Sleep(testingTimeForInnerMethod);
                 _tracer.StopTrace();
             }
 
             void OuterMethod()
             {
                 _tracer.StartTrace();
-                Thread.Sleep(300);
+                Thread.Sleep(testingTimeForOuterMethod);
                 InnerMethod();
                 _tracer.StopTrace();
             }
@@ -61,9 +69,82 @@ namespace TracerLibrary.Tests
             //Assert
             var traceResult = _tracer.GetTraceResult();
             var time = traceResult.Threads[Thread.CurrentThread.ManagedThreadId].Time;
-            var result = time.TotalMilliseconds is >= 800 and <= 810;
+            Assert.GreaterOrEqual(time.TotalMilliseconds, testingTimeForInnerMethod + testingTimeForOuterMethod, 
+                "Elapsed time must be equal or greater than expected.");
+            Assert.LessOrEqual(time.TotalMilliseconds, testingTimeForInnerMethod + testingTimeForOuterMethod + acceptableDeviation, 
+                "Elapsed time must be equal or less than acceptable deviation.");
+        }
+        
+        [Test]
+        public void Trace_Serial_Methods_300Ms_500Ms()
+        {
+            //Arrange
+            const int testingTimeForFirstMethod = 300;
+            const int testingTimeForSecondMethod = 500;
+            const int acceptableDeviation = 50;
             
-            Assert.IsTrue(result, "Traced time in one thread with inner method is incorrect.");
+            void FirstMethod()
+            {
+                _tracer.StartTrace();
+                Thread.Sleep(testingTimeForFirstMethod);
+                _tracer.StopTrace();
+            }
+
+            void SecondMethod()
+            {
+                _tracer.StartTrace();
+                Thread.Sleep(testingTimeForSecondMethod);
+                _tracer.StopTrace();
+            }
+            
+            //Act
+            FirstMethod();
+            SecondMethod();
+            
+            //Assert
+            var traceResult = _tracer.GetTraceResult();
+            var time = traceResult.Threads[Thread.CurrentThread.ManagedThreadId].Time;
+
+            Assert.GreaterOrEqual(time.TotalMilliseconds, testingTimeForFirstMethod + testingTimeForSecondMethod, 
+                "Elapsed time must be equal or greater than expected.");
+            Assert.LessOrEqual(time.TotalMilliseconds, testingTimeForFirstMethod + testingTimeForSecondMethod + acceptableDeviation, 
+                "Elapsed time must be equal or less than acceptable deviation.");
+        }
+        
+        
+        [Test]
+        public void Trace_TwoThreads_300Ms()
+        {
+            //Arrange
+            const int testingTime = 300;
+            const int acceptableDeviation = 50;
+            
+            void TestingMethod()
+            {
+                _tracer.StartTrace();
+                Thread.Sleep(testingTime);
+                _tracer.StopTrace();
+            }
+
+            var firstThread = new Thread(TestingMethod);
+            var secondThread = new Thread(TestingMethod);
+
+            //Act
+            firstThread.Start();
+            secondThread.Start();
+            Thread.Sleep(500);
+            
+            //Assert
+            var traceResult = _tracer.GetTraceResult();
+            Assert.AreEqual(traceResult.Threads.Count, 2, "There are must be two threadInformation structs in dictionary.");
+            foreach (var threadInformation in traceResult.Threads.Values)
+            {
+                var time = threadInformation.Time;
+                Assert.GreaterOrEqual(time.TotalMilliseconds, testingTime, 
+                    "Elapsed time must be equal or greater than expected.");
+                Assert.LessOrEqual(time.TotalMilliseconds, testingTime + acceptableDeviation, 
+                    "Elapsed time must be equal or less than acceptable deviation.");
+            }
         }
     }
 }
